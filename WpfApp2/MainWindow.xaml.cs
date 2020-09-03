91b4aa2310028c8e6ff91cb7b6553b44aa67964c
dudoser <frielender@gmail.com>
2020-09-03 23:17:49 +0700

    first commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+            db.Phones.Load();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach( Phone tmp in db.Phones)
+            {
+                Console.WriteLine(tmp.Title);
+            }
+        }
+    }
+}
